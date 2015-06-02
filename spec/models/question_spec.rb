29873c51d485c56b6854d270d7732417a19fe4bd
xnagpa <xnagpa@outlook.com>
2015-06-02 11:41:35 +0300

    Adds a model and some tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+   before(:each) do
+    @question = Question.new
+  end
+
+  it "should have title" do
+    @question.title = nil
+    @question.should_not be_valid
+  end
+
+   it "should have content" do
+    @question.content = nil
+    @question.should_not be_valid
+  end
+
 end
+
+
