3e0ce6aa839156c995fde6167d727dfb17eac26d
xnagpa <xnagpa@outlook.com>
2015-06-03 14:03:20 +0300

    Removes unnescessary migrations

--- db/migrate/20150602123344_add_parent_question_id_to_question.rb
+++
@@ -1,5 +0,0 @@
-class AddParentQuestionIdToQuestion < ActiveRecord::Migration
-  def change
-    add_column :questions, :parent_question_id, :integer
-  end
-end
--- db/migrate/20150602182810_remove_column_from_question.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromQuestion < ActiveRecord::Migration
-  def change
-  	remove_column :questions, :parent_question_id
-  end
-end
