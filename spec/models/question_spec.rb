7fce0203e2ef0252494d69a8b3315a2b22cfdfdf
xnagpa <xnagpa@outlook.com>
2015-07-17 13:29:39 +0300

    Adds tests of votable_module

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  let!(:user) { FactoryGirl.create(:user) }
   let!(:question) { FactoryGirl.create(:question_with_best_answer) }
+  let!(:positive_vote) { FactoryGirl.create(:vote, user:user) }
+  let!(:negative_vote) { FactoryGirl.create(:unvote) }
+  
+
   it {
     expect(subject).to validate_presence_of(:title)
   }
@@ -42,7 +47,33 @@ RSpec.describe Question, type: :model do
   it {
     expect(question.best_answer).to eq question.answers.where(best: true).first
   }
+
+  it {
+    expect(question.positive_votes).to eq Vote.where(votable_id: question.id, votable_type: question.class.name).sum(:thumb_up)
+  }
+
+  it {
+    expect(question.negative_votes).to eq Vote.where(votable_id: question.id, votable_type: question.class.name).sum(:thumb_down)
+  }
+
+  it {
+    expect(question.previous_vote(user)).to eq (Vote.where(user_id: user, votable_id: question.id, votable_type: question.class.name))
+  }
+
+  it {
+    expect(question.rating).to eq (question.positive_votes-question.negative_votes)
+  }
+
+  it {
+    expect(question.votes_are_different?(positive_vote,negative_vote)).to eq (true)
+  }
+
+  it {
+    expect(question.can_i_insert_this_vote?(negative_vote,user)).to eq (true)
+  }
   
+  
+ 
 
   # validates :title, presence: true
   # validates :title, length: {maximum:150}
